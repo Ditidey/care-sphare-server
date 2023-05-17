a178162e02e730ec9a6d5a0872d39338ec606e5d
Ditidey <ditidey2017@gmail.com>
2023-05-17 14:57:21 +0100

    updated

--- index.js
+++ index.js
@@ -24,34 +24,66 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const careSphare = client.db('care-sphare').collection('caresCollection');
-    const  registerLists = client.db('care-sphare').collection('registerLists');
+    const registerLists = client.db('care-sphare').collection('registerLists');
 
-     app.get('/allEvents', async(req, res)=>{
-          const result = await careSphare.find().toArray();
-          res.send(result);
-     })
-     app.get('/allEvents/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result =await careSphare.findOne(query);
-        res.send(result);
-     })
-     app.post('/addEvent', async(req, res)=>{
-        const events = req.body;
-        console.log(events)
-        const result = await careSphare.insertOne(events);
-        res.send(result);
-     })
-     
-     app.post('/registerLists', async(req, res)=>{
-        const lists = req.body;
-        console.log(lists);
-        const result = await registerLists.insertOne(lists);
-        res.send(result);
-     })
+    app.get('/allEvents', async (req, res) => {
+      const result = await careSphare.find().toArray();
+      res.send(result);
+    })
+    app.get('/allEvents/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await careSphare.findOne(query);
+      res.send(result);
+    })
+    app.post('/addEvent', async (req, res) => {
+      const events = req.body;
+      console.log(events)
+      const result = await careSphare.insertOne(events);
+      res.send(result);
+    })
 
+    app.post('/registerLists', async (req, res) => {
+      const lists = req.body;
+      console.log(lists);
+      const result = await registerLists.insertOne(lists);
+      res.send(result);
+    })
 
-
+    
+    app.get('/registerLists', async (req, res) => {
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      // console.log(query)
+      const result = await registerLists.find(query).toArray();
+      // if(result.length < 1){
+      //   res.send('please register for any event.')
+      // }
+      res.send(result)
+    })
+    app.get('/registerLists/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await registerLists.findOne(query);
+      res.send(result)
+    })
+    app.put('/registerLists/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const body = req.body;
+      const options = {upsert: true};
+      const updatedList = {
+        
+      }
+  })
+    app.delete('/registerLists/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await registerLists.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
@@ -64,11 +96,11 @@ async function run() {
 }
 run().catch(console.dir);
 
- 
-app.get('/', (req, res)=>{
-    res.send('care-sphare server is running');
+
+app.get('/', (req, res) => {
+  res.send('care-sphare server is running');
 })
 
-app.listen(port, ()=>{
-    console.log(`server running on ${port}`)
+app.listen(port, () => {
+  console.log(`server running on ${port}`)
 })
